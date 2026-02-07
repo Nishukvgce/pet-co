ef11f0fad85066e54a0694e489aaeabdbfb0ffbc
Nishukvgce <nishmitha928@gmail.com>
2026-02-07 16:43:30 +0530

    admin paenl isue

--- frontend/src/pages/admin-login/index.jsx
+++ frontend/src/pages/admin-login/index.jsx
@@ -22,7 +22,7 @@ const AdminLogin = () => {
     setLoading(true);
     setError('');
     try {
-      const res = await fetch('http://localhost:8081/api/auth/login', {
+      const res = await fetch('http://3.110.55.100:8081/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
